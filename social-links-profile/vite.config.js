47c729ece957879fc406a820bfa72e3bb551b236
LeoFernandez037 <leonelfernandezuwu@gmail.com>
2024-04-24 06:29:33 -0400

    ya fue x2

--- social-links-profile/vite.config.js
+++ social-links-profile/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://leofernandez037.github.io/social-links-profile/",
+  base: "https://leofernandez037.github.io/social-links-profile",
 });
